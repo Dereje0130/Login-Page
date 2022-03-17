830ef3589fab6d505a4f116e0686506dd22de257
Dereje <derejenet12@gmail.com>
2022-03-17 10:06:34 -0400

    new commit

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/new-login'
+app.use(express.static('./dist/Login-Page'
 
 ));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/new-login/'}),
+    res.sendFile('index.html', {root: 'dist/Login-Page/'}),
 );
 app.listen(process.env.PORT || 8080);
